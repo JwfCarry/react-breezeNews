0064de2ac03b4a048261554ece1559a235344ee8
jwf <2545538936@qq.com>
2022-09-06 12:27:48 +0800

    提交图片

--- src/views/news-sand-box/nopermission/Nopermission.js
+++ src/views/news-sand-box/nopermission/Nopermission.js
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import { Button, Result } from 'antd';
 export default function Nopermission() {
     return (
-        <div>
-           403  Nopermission
-        </div>
+        <Result
+            status="404"
+            title="404"
+            subTitle="Sorry, the page you visited does not exist."
+        />
     )
 }
